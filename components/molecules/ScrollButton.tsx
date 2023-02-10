230d0beca58368a52bf6f922862154e1f3e37800
MBJ <thiswallz@gmail.com>
2023-02-10 17:11:57 -0300

    minor fixes

--- components/molecules/ScrollButton.tsx
+++ components/molecules/ScrollButton.tsx
@@ -30,7 +30,7 @@ const ScrollButton = () => {
   return (
     <div className="fixed z-10 bottom-24 right-4 ">
       <Button
-        className="hover:bg-transparent"
+        className="hover:!bg-transparent"
         icon={
           <FaArrowCircleUp
             onClick={scrollToTop}
