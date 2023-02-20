27029318e190cd2a6d7c98f8ebbd99064fb9ffc5
Mar Tin <martinbarriaweb@gmail.com>
2023-02-20 18:09:52 -0300

    fix(FareWell): remove switch

--- components/organisms/FareWell.tsx
+++ components/organisms/FareWell.tsx
@@ -1,36 +1,16 @@
 import Image from 'next/image'
-import { useTranslation } from 'react-i18next'
 
 export default function FareWell() {
-  const { i18n } = useTranslation()
-
-  const supportUsText = () => {
-    switch (i18n.language) {
-      case 'es':
-        return (
-          <div>
-            <span className="text-red-500">Gracias </span>{' '}
-            <span className="text-yellow-500">por</span>{' '}
-            <span className="text-blue-500">apoyarnos</span>{' '}
-          </div>
-        )
-      default:
-        return (
-          <div>
-            <span className="text-red-500">Thank </span>{' '}
-            <span className="text-yellow-500">you</span>{' '}
-            <span className="text-blue-500">for</span>{' '}
-            <span className="text-purple-500">support</span>{' '}
-            <span className="text-cyan-500">us</span>
-          </div>
-        )
-    }
-  }
-
   return (
     <div className="py-8 w-full">
       <div className="text-center text-6xl font-bold pb-16">
-        {supportUsText()}
+        <div>
+          <span className="text-red-500">Thank </span>{' '}
+          <span className="text-yellow-500">you</span>{' '}
+          <span className="text-blue-500">for</span>{' '}
+          <span className="text-purple-500">support</span>{' '}
+          <span className="text-cyan-500">us</span>
+        </div>
       </div>
       <div className="pt-12 w-full flex  justify-center">
         <Image
