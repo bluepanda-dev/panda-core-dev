b1ca1fd0de5ac1eb08b1b05cfcbc72e41ff12c1c
Mar Tin <martinbarriaweb@gmail.com>
2023-02-21 13:48:16 -0300

    fix(uid): translations

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -4,7 +4,7 @@ import lottie from 'lottie-web'
 import animationData from '../data/lottie-animation.json'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
-import { useTranslation } from 'react-i18next'
+import { useTranslation } from 'next-i18next'
 import { useDataPages } from '@core/hooks/useDataPages'
 
 export default function Footer() {
