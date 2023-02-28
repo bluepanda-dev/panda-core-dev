e8381090ca747dcd4c553515e7cfba3a97c19597
MBJ <thiswallz@gmail.com>
2023-02-28 00:00:52 -0300

    headers credits

--- pages/orders.tsx
+++ pages/orders.tsx
@@ -1,9 +1,12 @@
 import * as dayjs from 'dayjs'
+import { useRouter } from 'next/router'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useState, useEffect } from 'react'
+import { FiArrowLeft } from 'react-icons/fi'
 import Button from '@components/atoms/Button'
 import Layout from '@components/layout'
 import Panel from '@components/molecules/Panel'
+import SimpleHeader from '@components/molecules/SimpleHeader'
 import { useCustomerContext } from '@core/contexts/CustomerContext'
 import { useUserContext } from '@core/contexts/UserContext'
 import { useCopyPages } from '@core/hooks/useCopyPages'
@@ -12,6 +15,7 @@ import { ProductCard, Order } from '@core/types'
 type HydratedProduct = ProductCard & { order: Order }
 
 const Orders = () => {
+  const router = useRouter()
   const { profile } = useUserContext()
   const { orders } = useCustomerContext()
   const { products } = useCopyPages()
@@ -27,6 +31,10 @@ const Orders = () => {
     if (invoice) {
       window.open(invoice, '_blank')
     }
+  }
+
+  function back() {
+    router.push('/')
   }
 
   async function fetchOwnProducts() {
@@ -81,64 +89,77 @@ const Orders = () => {
   }
 
   return (
-    <Layout className="flex justify-center">
-      <div className="mx-2 md:mx-8 my-16 h-full max-w-2xl w-full">
-        <div className="text-center text-4xl font-bold">My Orders</div>
-        <div className="mt-8 flex flex-col gap-6 justify-center">
-          {ownProducts.map((product) => (
-            <Panel
-              key={product.order.id}
-              title={product.title}
-              description={product.description}
-              hints={
-                <span>
-                  Purchased on{' '}
-                  {dayjs
-                    .unix(Number(product.order.created))
-                    .format('DD/MM/YYYY')}
+    <Layout className="justify-center">
+      <SimpleHeader
+        title={
+          <div className="flex items-center gap-4">
+            <FiArrowLeft
+              className="cursor-pointer hover:opacity-75"
+              onClick={back}
+            />
+            <div className="text-center text-xl font-bold">My Orders</div>
+          </div>
+        }
+      />
+
+      <div className="flex justify-center">
+        <div className="mx-2 md:mx-8 my-16 h-full max-w-2xl w-full">
+          <div className="mt-8 flex flex-col gap-6 justify-center">
+            {ownProducts.map((product) => (
+              <Panel
+                key={product.order.id}
+                title={product.title}
+                description={product.description}
+                hints={
+                  <span>
+                    Purchased on{' '}
+                    {dayjs
+                      .unix(Number(product.order.created))
+                      .format('DD/MM/YYYY')}
+                  </span>
+                }
+                footer={
+                  <>
+                    {product.order.invoice && (
+                      <Button
+                        onClick={() => handleInvoice(product.order.invoice)}
+                        className="w-48"
+                        isSmall={true}
+                      >
+                        Invoice
+                      </Button>
+                    )}
+                    {product.order.protectedItem?.download && (
+                      <Button
+                        onClick={() =>
+                          handleDownload(product.order.protectedItem.download)
+                        }
+                        isInverted={true}
+                        className="w-48"
+                        isSmall={true}
+                      >
+                        Download
+                      </Button>
+                    )}
+                  </>
+                }
+              >
+                <span className="absolute right-2 top-2 dark:bg-normal-700 dark:text-neutral-300 rounded-lg p-1">
+                  {product.price}
                 </span>
-              }
-              footer={
-                <>
-                  {product.order.invoice && (
-                    <Button
-                      onClick={() => handleInvoice(product.order.invoice)}
-                      className="w-48"
-                      isSmall={true}
-                    >
-                      Invoice
-                    </Button>
-                  )}
-                  {product.order.protectedItem?.download && (
-                    <Button
-                      onClick={() =>
-                        handleDownload(product.order.protectedItem.download)
-                      }
-                      isInverted={true}
-                      className="w-48"
-                      isSmall={true}
-                    >
-                      Download
-                    </Button>
-                  )}
-                </>
-              }
-            >
-              <span className="absolute right-2 top-2 dark:bg-normal-700 dark:text-neutral-300 rounded-lg p-1">
-                {product.price}
-              </span>
-              {product.order.protectedItem?.raw && (
-                <div>
-                  <div className="text-primary-600 font-semibold my-4">
-                    Unlocked content:
+                {product.order.protectedItem?.raw && (
+                  <div>
+                    <div className="text-primary-600 font-semibold my-4">
+                      Unlocked content:
+                    </div>
+                    <div className="bg-neutral-200 dark:bg-neutral-800 p-8">
+                      <code>{product.order.protectedItem?.raw}</code>
+                    </div>
                   </div>
-                  <div className="bg-neutral-200 dark:bg-neutral-800 p-8">
-                    <code>{product.order.protectedItem?.raw}</code>
-                  </div>
-                </div>
-              )}
-            </Panel>
-          ))}
+                )}
+              </Panel>
+            ))}
+          </div>
         </div>
       </div>
     </Layout>
