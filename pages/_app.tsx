c44b9e3a76ab415b4eeaf02664c2d666c6868342
MBJ <thiswallz@gmail.com>
2023-03-14 22:28:05 -0300

    custom css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import '../styles/custom.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { appWithTranslation } from 'next-i18next'
